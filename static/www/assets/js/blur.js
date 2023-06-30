3064eb7f3a4c632a2d84cb3c4eb8aec5cbdb4798
robinmuff <robin.muff@bluewin.ch>
2023-06-30 11:15:29 +0200

    Add load blur

---
+++ static/www/assets/js/blur.js
@@ -0,0 +1,17 @@
+const loadText = document.querySelector(".loading-text");
+const bg = document.querySelector(".container");
+
+let load = 0;
+
+let interval = setInterval(blurring, 10);
+
+function blurring() {
+  load++;
+  if (load > 99) {
+    clearInterval(interval);
+  }
+  loadText.innerText = `${load}%`;
+  loadText.style.opacity = 1 - load / 100;
+  loadText.style.width = `${10 + load}%`;
+  bg.style.filter = `blur(${30 - (load * 30) / 100}px)`;
+}
